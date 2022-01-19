a7e430b4d05572868996db2dad3b1e468c7b2e39
EngAbdelrahmanMagdi <eng.abdelrahman.magdi@gmail.com>
2022-01-19 22:44:04 +0200

    create figure class

--- Project Framework/Figures/CFigure.h
+++ Project Framework/Figures/CFigure.h
@@ -11,19 +11,20 @@ protected:
 	int ID;		//Each figure has an ID
 	bool Selected;	//true if the figure is selected.
 	GfxInfo FigGfxInfo;	//Figure graphis info
-	
+
 	/// Add more parameters if needed.
 
 public:
 	CFigure(GfxInfo FigureGfxInfo);
-	void SetSelected(bool );	//select/unselect the figure
+	void SetSelected(bool);	//select/unselect the figure
 	bool IsSelected() const;	//check whether fig is selected
 
-	virtual void DrawMe(GUI*) const  = 0 ;		//Draw the figure
-	
+	virtual void DrawMe(GUI*) const = 0;		//Draw the figure
+
+
 	void ChngDrawClr(color Dclr);	//changes the figure's drawing color
 	void ChngFillClr(color Fclr);	//changes the figure's filling color
-
+	virtual bool PointInShape(int x, int y) const = 0;
 	///The following functions should be supported by the figure class
 	///It should be overridden by each inherited figure
 
